2bb2aa4a26711246e0a3c7b9418ba1b2c707c69b
Haritz Saiz <haritz1997@gmail.com>
2025-06-14 09:54:17 +0000

    Error: Page "/dashboard/certificate-authorities/[caId]/details/page" is

--- src/app/dashboard/certificate-authorities/[caId]/issue-certificate/page.tsx
+++ src/app/dashboard/certificate-authorities/[caId]/issue-certificate/page.tsx
@@ -12,6 +12,7 @@ export async function generateStaticParams() {
     { caId: '07c23e39-964a-4a2b-8021-e9584c4db8ad' }, // Example from API
     { caId: '3f3b381b-ed94-4017-ade9-76f2958b39e6' }, // Example from API
     { caId: '073e35ae-de5a-4183-a7a6-2125600bda5f' }, // New ID from build error
+    { caId: 'c82d689b-3c4f-454f-92e4-23d0d07c00a1' }, // Added this ID
     // Add other known/important CA IDs here if needed for pre-rendering
   ];
 }
