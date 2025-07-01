08971bb1cc8e7ddbca1f931a7b06f61e1e649055
Hariz Saiz <hsaiz@ikerlan.es>
2025-07-01 17:58:19 +0000

    When save VA config call /api/va/roles/X where X is the CA SubjectKeyID

--- src/lib/api-domains.ts
+++ src/lib/api-domains.ts
@@ -6,3 +6,4 @@ export const DEV_MANAGER_API_BASE_URL = `${API_BASE_URL}/devmanager/v1`;
 export const DMS_MANAGER_API_BASE_URL = `${API_BASE_URL}/dmsmanager/v1`;
 export const ALERTS_API_BASE_URL = `${API_BASE_URL}/alerts/v1`;
 export const EST_API_BASE_URL = `${API_BASE_URL}/dmsmanager/.well-known/est`;
+export const VA_API_BASE_URL = `${API_BASE_URL}/va/v1`;
