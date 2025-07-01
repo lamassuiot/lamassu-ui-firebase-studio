c705fe060cdd264e2ef36db5d171836c51063905
Hariz Saiz <hsaiz@ikerlan.es>
2025-07-01 10:01:53 +0000

    make Device Status Overview less wide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -155,11 +155,11 @@ export default function HomePage() {
 
   return (
     <div className="w-full space-y-8">
-      <div className="grid grid-cols-1 xl:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-8">
         <div className="xl:col-span-1">
           <SummaryStatsCard stats={summaryStats} isLoading={isLoadingStats || authLoading} />
         </div>
-        <div className="xl:col-span-2">
+        <div className="xl:col-span-1">
           <DeviceStatusChartCard />
         </div>
       </div>
