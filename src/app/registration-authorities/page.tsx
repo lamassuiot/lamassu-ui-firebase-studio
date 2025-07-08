38b7b6b8128b44b1d6935201050ebeb6a4ad65c5
Juan José Rodríguez <jjrodriguez@lksnext.com>
2025-07-08 08:14:20 +0000

    Change "EST Actions" by "EST (RFC-7030)"

--- src/app/registration-authorities/page.tsx
+++ src/app/registration-authorities/page.tsx
@@ -282,7 +282,7 @@ export default function RegistrationAuthoritiesPage() {
                                         <DropdownMenuSub>
                                             <DropdownMenuSubTrigger>
                                                 <TerminalSquare className="mr-2 h-4 w-4" />
-                                                <span>EST Actions</span>
+                                                <span>EST (RFC-7030)</span>
                                             </DropdownMenuSubTrigger>
                                             <DropdownMenuPortal>
                                                 <DropdownMenuSubContent>
