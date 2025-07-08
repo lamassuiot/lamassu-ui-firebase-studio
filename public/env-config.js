107568e86b9e7e06a7302b66aa209a2535ef870d
Juan José Rodríguez <jjrodriguez@lksnext.com>
2025-07-08 19:51:34 +0000

    load env-config.js at app startup and use value from LAMASSU_API to set

---
+++ public/env-config.js
@@ -0,0 +1,7 @@
+// This file is loaded at application startup to provide runtime configuration.
+// You can edit this file to change backend API endpoints without rebuilding the application.
+
+window.lamassuConfig = {
+  // The base URL for all backend API services.
+  LAMASSU_API: 'https://lab.lamassu.io/api'
+};
