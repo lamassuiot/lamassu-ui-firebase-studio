22566b0babae6d73597e9ac76c4b85c8d1fc8f46
Juanjo Rodriguez <jjrodriguez@lksnext.com>
2025-07-18 16:47:41 +0200

    fix: fix sample theme (#35)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -21,7 +21,7 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("bg-muted/50", "[&_tr]:border-b", className)} {...props} />
+  <thead ref={ref} className={cn("bg-[--table-header-background]", "[&_tr]:border-b", className)} {...props} />
 ))
 TableHeader.displayName = "TableHeader"
 
@@ -74,7 +74,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 text-left align-middle font-medium text-[--table-header-foreground] [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
